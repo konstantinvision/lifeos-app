c899a1111f74167e28cbe2c33ca12de2487c86e7
konstantinvision <2299kd@gmail.com>
2026-04-29 12:50:02 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Life OS Service Worker — v9.6
-const CACHE_NAME = "lifeos-v9-6";
+// Life OS Service Worker — v9.7
+const CACHE_NAME = "lifeos-v9-7";
 const ALWAYS_FRESH = ["index.html", "sw.js", "./", "/"];
 
 self.addEventListener("install", e => {
