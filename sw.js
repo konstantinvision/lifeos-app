aa53938f274d7f22074210a725efd54f57438ff7
konstantinvision <2299kd@gmail.com>
2026-04-28 23:11:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lifeos-v10';
+const CACHE = 'lifeos-v9.3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
