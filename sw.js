d7f49ef875a2d90a28e3f0e15f755d3cd2abf318
konstantinvision <2299kd@gmail.com>
2026-07-02 12:49:58 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Life OS Service Worker — v25
 // ⚠️ CACHE_NAME — меняй ЭТУ строку при каждом обновлении index.html
-const CACHE_NAME = "lifeos-v53";
+const CACHE_NAME = "lifeos-v54";
 
 self.addEventListener("install", e => {
   self.skipWaiting();
